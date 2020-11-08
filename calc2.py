182a0da0e84c7fc34252bada1c90f90a5c8572b1
cyberpayload <will@cyberpayload.com>
2020-11-08 15:39:25 -0600

    setting up commented sections

---
+++ calc2.py
@@ -0,0 +1,15 @@
+# ********** A CALCULATOR WITH A LOOP **********
+
+# create while loop
+
+# display calculator menu to user
+
+# get user choice on operation
+
+# get user numbers for the operation
+
+# check operation variable
+
+# pass in operation functions
+
+

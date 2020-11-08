02c9aff8b08bd5377cd21766af6aaa135d466c3c
cyberpayload <will@cyberpayload.com>
2020-11-08 16:05:52 -0600

    added user operation variable, numbers for operation, and operation check

--- calc2.py
+++ calc2.py
@@ -10,11 +10,21 @@ print('4. DIVIDE')
 print('TYPE Q TO QUIT')
 
 # get user choice on operation
-
+operation = input()
 
 # get user numbers for the operation
+num1 = input()
+num2 = input()
 
 # check operation variable
+if operation == '1':
+    add(num1,num2)
+elif operation == '2':
+    sub(num1,num2):
+elif operation == '3':
+    mult(num1,num1)
+elif operation == '4':
+    div(num1,num2)
 
 # pass in operation functions
 
